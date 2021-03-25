d49cb3ee8cb48052f9b1ab42b390cbf749e0f94f
ran <284008820@qq.com>
2021-03-25 22:10:51 +0800

    CoordinatorLayout AppBarLayout

--- app/src/main/java/com/caogd/myejiao/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/caogd/myejiao/ui/notifications/NotificationsFragment.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.caogd.myejiao.R
+import com.caogd.myejiao.databinding.FragmentNotificationsBinding
 
 class NotificationsFragment : Fragment() {
 
@@ -21,11 +21,13 @@ class NotificationsFragment : Fragment() {
     ): View? {
         notificationsViewModel =
                 ViewModelProvider(this).get(NotificationsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+
+        val binding = FragmentNotificationsBinding.inflate(inflater)
+
+        val mActivity = activity as AppCompatActivity
+        mActivity.setSupportActionBar(binding.toolbar)
+        val actionBar : ActionBar? = mActivity.supportActionBar
+        actionBar?.title = "Notification!"
+        return binding.root
     }
 }
