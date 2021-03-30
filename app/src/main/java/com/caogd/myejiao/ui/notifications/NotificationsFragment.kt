95375d8d02510e0191ba523f05816c80e3dc8fc5
ran <284008820@qq.com>
2021-03-30 21:33:56 +0800

    CardView实现圆角头像

--- app/src/main/java/com/caogd/myejiao/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/caogd/myejiao/ui/notifications/NotificationsFragment.kt
@@ -1,14 +1,18 @@
 package com.caogd.myejiao.ui.notifications
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.caogd.myejiao.R
 import com.caogd.myejiao.databinding.FragmentNotificationsBinding
+import com.google.android.material.appbar.AppBarLayout
 
 class NotificationsFragment : Fragment() {
 
@@ -27,7 +31,36 @@ class NotificationsFragment : Fragment() {
         val mActivity = activity as AppCompatActivity
         mActivity.setSupportActionBar(binding.toolbar)
         val actionBar : ActionBar? = mActivity.supportActionBar
-        actionBar?.title = "Notification!"
+        actionBar?.title = ""
+
+        val totalHeight = resources.getDimension(R.dimen.bar_height)
+        val toolBarHeight = resources.getDimension(R.dimen.action_height)
+        val photoHeight = resources.getDimension(R.dimen.photo_height_out)
+        val offSetHeight = totalHeight - toolBarHeight
+        val tmp = (toolBarHeight - photoHeight) / 2
+        var rlOffDistance = 0;
+        binding.appbar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
+            val layoutParam: FrameLayout.LayoutParams = binding.llUser.layoutParams as FrameLayout.LayoutParams
+            Log.i("tag", "verticalOffset = $verticalOffset , $tmp")
+            //计算margin top值
+            if (rlOffDistance == 0){
+                rlOffDistance = layoutParam.topMargin
+            }
+
+            //得到滑动比值
+            val rlOffDistanceScale = rlOffDistance / offSetHeight
+
+            //计算margin top值
+            var distance = rlOffDistance - (-verticalOffset) * rlOffDistanceScale
+            Log.i("tag", "distance = $distance")
+            if (distance <= tmp){
+                distance = tmp
+            }
+            layoutParam.topMargin = distance.toInt()
+
+            binding.llUser.layoutParams = layoutParam
+        })
+
         return binding.root
     }
 }
